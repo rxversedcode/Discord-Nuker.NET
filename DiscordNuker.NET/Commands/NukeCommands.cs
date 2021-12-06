ede8e82cd68b535be8e1212c1195c1bf973c5bc6
rxversed <rxversedmc@protonmail.com>
2021-12-05 19:06:58 -0800

    buildable again

--- DiscordNuker.NET/Commands/NukeCommands.cs
+++ DiscordNuker.NET/Commands/NukeCommands.cs
@@ -11,7 +11,7 @@ namespace DiscordNuker.NET.Commands
 {
     public class NukeCommands : ModuleBase
     {
-        //Look at trello board!
+        //Look at trello board!net5.0
 
         [Command("mass chan", RunMode = RunMode.Async)]
 
@@ -29,7 +29,6 @@ namespace DiscordNuker.NET.Commands
             string channame = Console.ReadLine();
             var count = Stopwatch.StartNew();
             count.Start();
-
             for (int i = 0; i < num; i++)
             {
                 try
@@ -45,6 +44,7 @@ namespace DiscordNuker.NET.Commands
             }
             count.Stop();
             Console.WriteLine("Made " + num + " channels in " + count.ElapsedMilliseconds);
+
         }
 
         [Command("mass del", RunMode = RunMode.Async)]
